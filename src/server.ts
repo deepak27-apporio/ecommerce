c13e953eb8c152fb8616d71cfdcb10f7a712fd48
deepak <deepak.sahu@apporio.in>
2026-05-07 16:33:29 +0530

    add frontend url for cores

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000","https://ecommerce-frontend-lpaw.onrender.com"],
     credentials: true,
   }),
 );
