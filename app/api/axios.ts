4a0f889843e852925656e1da870f2843b8980d86
deepak <deepak.sahu@apporio.in>
2026-05-07 17:49:23 +0530

    fix cookie issue

--- app/api/axios.ts
+++ app/api/axios.ts
@@ -6,7 +6,7 @@ import { getToken, setToken, clearToken } from "./token";
 const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const api = axios.create({
-  baseURL: API_URL,
+  baseURL: "/api",
   withCredentials: true,
 });
 
